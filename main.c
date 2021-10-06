bc0c94f09342c9b818b3c752d74561cd3452c1c3
alessandroTola <alessandro.tola@yahoo.it>
2021-10-06 16:34:55 +0200

    divisione corretta

--- main.c
+++ main.c
@@ -53,3 +53,10 @@ float sottrazione(float a, float b){
 float moltiplicazione(float a, float b){
     return a-b;
 }
+
+float divisione(float a, float b){
+    if(b != 0)
+        return a/b;
+    else
+        return -1;
+}
